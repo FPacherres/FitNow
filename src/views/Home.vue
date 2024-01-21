5e3071259e04a908c6a7d7de7ab50cf9be1e12a3
FPacherres <fpacherres.tech@gmail.com>
2024-01-21 01:41:55 -0500

    style iphone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,7 +59,7 @@
     grid-template-rows: 40px 1fr;
   }
   .routineBtns {
-    height: calc(100vh - 200px);
+    height: calc(100vh - 300px);
     display: flex;
     flex-direction: column;
     gap: 10px;
